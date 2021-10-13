66182fb420b4a5f1ed7dbd7eca26986ec8d78358
Jacob <Jacobonasrallah@gmail.com>
2021-10-12 17:49:43 -0700

    commit tree b9c4ea9027133d02ff7dd316fa520bc257efbb46

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    #setter function
+    def name=(name)
+        @name = name
+    end
+    #getter function
+    def name
+        @name
+    end
 
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+
+    
 end
